3b0a623a0a355041f583a17014730f1bb97a5533
kubinyiandras <kubinyi.andras@gmail.com>
2018-09-21 15:35:03 +0200

    első részfeladat

--- gfbewiubgfvwe9uibgw/Program.cs
+++ gfbewiubgfvwe9uibgw/Program.cs
@@ -10,6 +10,24 @@ namespace gfbewiubgfvwe9uibgw
     {
         static void Main(string[] args)
         {
+            //1 feladat
+            Console.WriteLine("Első feladat: ");
+            int[] tomb1 = new int[10];
+            int max1 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Írjob be még {0} számot!", +10-1);
+                
+                tomb1[i] = Convert.ToInt32(Console.ReadLine());
+                if (tomb1[i]>max1)
+                {
+                    max1 = tomb1[i];
+                }
+            }
+            Console.WriteLine("Legnagyobb szám: "+max1);
+            
+
+            Console.ReadLine();
         }
     }
 }
