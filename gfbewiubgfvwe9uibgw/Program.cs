f3be179dfbc073add467beba0837c21cc3aa5321
kubinyiandras <kubinyi.andras@gmail.com>
2018-09-21 15:35:39 +0200

    második részfeladat

--- gfbewiubgfvwe9uibgw/Program.cs
+++ gfbewiubgfvwe9uibgw/Program.cs
@@ -25,7 +25,27 @@ namespace gfbewiubgfvwe9uibgw
                 }
             }
             Console.WriteLine("Legnagyobb szám: "+max1);
-            
+
+            //2. feladat
+            Console.WriteLine("Második feladat: ");
+            Console.WriteLine("Kérem adja meg hogy hány számot szeretne megadni: ");
+            int db = Convert.ToInt32(Console.ReadLine());
+            int[] tomb2 = new int[10];
+            int max2 = 0;
+            for (int i = 0; i < db; i++)
+            {
+                Console.WriteLine("Írjob be még {0} számot!", +10 - 1);
+
+                tomb2[i] = Convert.ToInt32(Console.ReadLine());
+                if (tomb2[i] > max2)
+                {
+                    max2 = tomb2[i];
+                }
+            }
+            Console.WriteLine("Legnagyobb szám: " + max2);
+
+ 
+
 
             Console.ReadLine();
         }
